d16b1e51f05eda31df47c0c198b4d47f921cd06d
chinchien <a109253046@mail.shu.edu.tw>
2024-04-12 16:00:39 +0800

    0411

---
+++ CH5_4/app/src/main/java/com/example/ch5_4/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ch5_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onKeyDown(int KeyCode, KeyEvent event){
+        if (KeyCode==KeyEvent.KEYCODE_BACK){
+            EditText txtinput=(EditText) findViewById(R.id.txtInput);
+            txtinput.setText("按下BACK按鍵");
+            return true;
+        }
+        return super.onKeyDown(KeyCode,event);
+
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode,KeyEvent event){
+        TextView output=(TextView) findViewById(R.id.lblOutput);
+        output.setText("按下按鍵碼:"+keyCode);
+        return super.onKeyUp(keyCode,event);
+    }
+
+}
