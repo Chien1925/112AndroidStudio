11ee47bc8c5b008fc158612a0f2ec77fdfcd84d2
CC <a109253046@mail.shu.edu.tw>
2024-05-16 15:47:56 +0800

    0516

--- 0509/ArrayAdapterSpinner/app/src/main/java/com/example/arrayadapterspinner/MainActivity.java
+++ 0509/ArrayAdapterSpinner/app/src/main/java/com/example/arrayadapterspinner/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.arrayadapterspinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -17,5 +20,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txv=(TextView) findViewById(R.id.order);
+        temp=(Spinner) findViewById(R.id.temp);
+
+        drink=(Spinner) findViewById(R.id.drink);
+        drink.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[]tempSet;
+                if(position==3)
+                    tempSet=tempset2;
+                else
+                    tempSet=tempset1;
+
+                ArrayAdapter<String>tempAd=
+                        new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item,tempSet);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
