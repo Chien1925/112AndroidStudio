642e76cadada71325fa1405c879fc13df0fd89a5
CC <a109253046@mail.shu.edu.tw>
2024-05-16 17:10:33 +0800

    0516

---
+++ 0516/AlertDialogDemo/app/src/main/java/com/example/alertdialogdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.alertdialogdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnAbout=(Button) findViewById(R.id.btnAbout);
+        btnAbout.setOnClickListener(v -> {
+            new AlertDialog.Builder(this)
+                    .setTitle("關於本書")
+                    .setMessage("Android程式設計與應用\n作者：陳會安\n教師：恰克虎")
+                    .setCancelable(true)
+                    .setPositiveButton("確定",null)
+                    .show();
+        });
+    }
+}
