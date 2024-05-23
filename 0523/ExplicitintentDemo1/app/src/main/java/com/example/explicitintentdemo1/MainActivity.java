2501ee9dda6f92a084b5a943c2881619e561c1e9
CC <a109253046@mail.shu.edu.tw>
2024-05-23 17:39:51 +0800

    0523

--- 0523/ExplicitintentDemo1/app/src/main/java/com/example/explicitintentdemo1/MainActivity.java
+++ 0523/ExplicitintentDemo1/app/src/main/java/com/example/explicitintentdemo1/MainActivity.java
@@ -21,9 +21,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 EditText txtC=(EditText) findViewById(R.id.txtC);
+                int tempC=Integer.parseInt(txtC.getText().toString());
+                EditText edtPlace=(EditText)findViewById(R.id.edtPlace);
                 Intent intent=new Intent(MainActivity.this,FActivity.class);
                 Bundle bundle=new Bundle();
-                bundle.putString("TEMPC",txtC.getText().toString());
+                bundle.putInt("TEMPC",tempC);
+                bundle.putString("PALACE",edtPlace.getText().toString());
                 intent.putExtras(bundle);
                 startActivity(intent);
             }
