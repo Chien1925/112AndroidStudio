d7f1c2ad4b88071b9d9c3607f77c2bcf0638ebbb
chinchien <a109253046@mail.shu.edu.tw>
2024-06-13 16:36:08 +0800

    0613

--- BMI/app/src/main/java/com/example/bmi/MainActivity.java
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -16,36 +16,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txvShow=(TextView) findViewById(R.id.txvShow);
+        TextView txvShow = (TextView) findViewById(R.id.txvShow);
         txvShow.setTextSize(36);
 
-        Button btnCount=(Button) findViewById(R.id.btnCount);
-        Button btnClear=(Button) findViewById(R.id.btnClear);
+        Button btnCount = (Button) findViewById(R.id.btnCount);
+        Button btnClear = (Button) findViewById(R.id.btnClear);
         btnCount.setOnClickListener(this);
         btnClear.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        EditText edtHeight =(EditText) findViewById(R.id.edtHeight);
-        EditText edtWeight =(EditText) findViewById(R.id.edtWeight);
-        TextView txvShow=(TextView) findViewById(R.id.txvShow);
+        EditText edtHeight = (EditText) findViewById(R.id.edtHeight);
+        EditText edtWeight = (EditText) findViewById(R.id.edtWeight);
+        TextView txvShow = (TextView) findViewById(R.id.txvShow);
         txvShow.setTextSize(36);
 
-        if(v.getId() == R.id.btnCount){
-            double height=Double.parseDouble((edtHeight.getText().toString()));
-            double weight=Double.parseDouble((edtWeight.getText().toString()));
-            double bmi = weight/Math.pow(height/100.0,2);
-            if(bmi>=24)
+        if (v.getId() == R.id.btnCount) {
+            double height = Double.parseDouble((edtHeight.getText().toString()));
+            double weight = Double.parseDouble((edtWeight.getText().toString()));
+            
+            if (height == 0 || weight == 0) {
                 txvShow.setTextColor(Color.RED);
-            else if(bmi<18.5)
-                txvShow.setTextColor(Color.BLUE);
-            else
-                txvShow.setTextColor(Color.GREEN);
+                txvShow.setText("錯誤");
+            } else {
+                double bmi = weight / Math.pow(height / 100.0, 2);
+                if (bmi >= 24)
+                    txvShow.setTextColor(Color.RED);
+                else if (bmi < 18.5)
+                    txvShow.setTextColor(Color.BLUE);
+                else
+                    txvShow.setTextColor(Color.GREEN);
 
-            txvShow.setText(String.format("%.2f",bmi));
-        }
-        else{
+                txvShow.setText(String.format("%.2f", bmi));
+            }
+        } else {
             edtWeight.setText("0");
             edtHeight.setText("0");
             txvShow.setText("");
